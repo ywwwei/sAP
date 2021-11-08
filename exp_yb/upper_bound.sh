6786419fa27c3c50ef8ca9af8c8f53dd75aca9a0
ywwwei <2015212903@bupt.edu.cn>
2021-11-08 11:04:48 -0600

    Add

---
+++ exp_yb/upper_bound.sh
@@ -0,0 +1,13 @@
+# For documentation, please refer to "doc/tasks.md"
+
+dataDir="/nobackup/yb"
+
+methodName=mrcnn50_nm
+scale=0.5
+# "nm" is short for "no mask"
+
+python det/eval_upper_bound.py \
+	--annot-path "$dataDir/Argoverse-HD/annotations/val.json" \
+	--no-mask \
+	--out-dir "$dataDir/Exp/Argoverse-HD/output/${methodName}_s${scale}/val" \
+	--overwrite \
